1ae935cbd8f8a858a48809730bc34be6e466bd23
Lars Gerrit Kliesing <lgk@patorg.org>
2019-03-06 15:30:52 +0100

    public website

---
+++ docs/precache-manifest.7e1bcccc0600fe34a84d9815df2c636e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "928e6a46097100263bd1",
+    "url": "/demo-app/static/js/runtime~main.928e6a46.js"
+  },
+  {
+    "revision": "7f3dec1b53383583aea4",
+    "url": "/demo-app/static/js/main.7f3dec1b.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/demo-app/static/js/2.ac164eeb.chunk.js"
+  },
+  {
+    "revision": "7f3dec1b53383583aea4",
+    "url": "/demo-app/static/css/main.8a429ba5.chunk.css"
+  },
+  {
+    "revision": "92ec21e640c0254231a032cf62f3a13e",
+    "url": "/demo-app/index.html"
+  }
+];
